ff772f2defd63a49aa890aa4c5738a37ed7557dc
Jörg <jbantin@gmx.de>
2024-11-18 11:15:26 +0100

    ver0.1

--- src/components/KeyBoard.jsx
+++ src/components/KeyBoard.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import bassDrum from "../assets/sounds/BassDrum[BD]/BD06.wav";
+import bassDrum2 from "../assets/sounds/BassDrum[BD]/E808_BD[short]-02.wav";
 const KeyBoard = () => {
   const bd = new Audio(bassDrum);
-  const bd2 = new Audio(bassDrum);
+  const bd2 = new Audio(bassDrum2);
   console.log(bd);
   const play = () => {
     bd.play();
